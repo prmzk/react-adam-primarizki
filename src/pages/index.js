a90e954e0f15951ce66bdd2362c41be3d6523910
Adam Primarizki <adam.primarizki@gmail.com>
2021-10-16 17:20:50 +0700

    Styling and SASS init, folders init

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div>
+      <div>asd</div>
+      <h1>asd</h1>
+      <h2>asd</h2>
+      <h3>asd</h3>
+      <h4>asd</h4>
+      <h5>asd</h5>
+      <a>asd</a>
+      <p>asd</p>
+    </div>
+  )
+}
+
+export default App;
